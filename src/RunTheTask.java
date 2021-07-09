1f07147dff2e9567d2e8b88b1e8ba54d419cbd70
GevorgBagratunyan <g.bagratunyan@gmail.com>
2021-07-09 17:01:29 +0400

    added util class named DataSorter

--- src/RunTheTask.java
+++ src/RunTheTask.java
@@ -1,21 +1,22 @@
-import insertionsort.InsertionSort;
-import quicksort.QuickSort;
+
+
+import sortingalgorithms.DataSorter;
 
 import java.util.Arrays;
 
 public class RunTheTask {
     public static void main(String[] args) {
-        int arr1[] = {4,-1,6,8,0,5,-3};
-        int arr2[] = {3,3,3,4,-1,6,8,0,5,-3};
-
-        QuickSort qs = new QuickSort();
-        qs.quickSort(arr1,0,arr1.length-1);
-
-        InsertionSort is = new InsertionSort();
-        is.insertionSort(arr2);
+        int arr1[] = {-3,-3,4,-1,6,8,0,5,-3};
+        int arr2[] = {3,3,3,12,-8,4,-1,6,8,0,5,-3};
 
 
-        System.out.println(Arrays.toString(arr1));
-        System.out.println(Arrays.toString(arr2));
+        System.out.println("Array 1 BEFORE sort :\n" + Arrays.toString(arr1));
+        DataSorter.insertionSort(arr1);
+        System.out.println("Array 1 AFTER sort using -Insertion Sort- :\n" + Arrays.toString(arr1) + "\n");
+
+        System.out.println("Array 2 BEFORE sort :\n" + Arrays.toString(arr2));
+        DataSorter.quickSort(arr2,0,arr2.length-1);
+        System.out.println("Array 2 AFTER sort using -QuickSort- :\n" + Arrays.toString(arr2) + "\n");
+
     }
 }
--- src/sortingalgorithms/DataSorter.java
+++ src/sortingalgorithms/DataSorter.java
@@ -1,9 +1,28 @@
-package quicksort;
+package sortingalgorithms;
 
+public class DataSorter {
 
-public class QuickSort {
-    //selects last element as pivot, pi using which array is partitioned.
-    int partition(int intArray[], int low, int high) {
+    private DataSorter(){}
+
+    //Insertion Sort
+    //___________________________________________________________________________
+    public static void insertionSort(int[] arr) {
+        int i, j, key, temp;
+        for (i = 1; i < arr.length; i++) {
+            key = arr[i];
+            j = i - 1;
+            while (j >= 0 && key < arr[j]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                j--;
+            }
+        }
+    }
+
+    //QuickSort
+    //___________________________________________________________________________
+    private static int partition(int intArray[], int low, int high) {
         int pi = intArray[high];
         int i = (low-1); // smaller element index
         for (int j=low; j<high; j++) {
@@ -27,7 +46,7 @@ public class QuickSort {
 
 
     //routine to sort the array partitions recursively
-    public void quickSort(int intArray[], int low, int high) {
+    public static void quickSort(int intArray[], int low, int high) {
         if (low < high) {
             //partition the array around pi=>partitioning index and return pi
             int pi = partition(intArray, low, high);
