601472c36a804e8fb608e88ec0da2d26fb0f7024
GevorgBagratunyan <g.bagratunyan@gmail.com>
2021-07-11 15:19:56 +0400

    Added additional GenericArrayUtil class

--- src/anagramchecker/Main.java
+++ src/anagramchecker/Main.java
@@ -15,25 +15,15 @@ public class Main {
 
         boolean isAnagrams1 = StringUtil.isAnagram(a, b);
         boolean isAnagrams2 = StringUtil.isAnagram(c, d);
-
         printResult(a, b, isAnagrams1);
         printResult(c, d, isAnagrams2);
 
-
         //Same actions using generic array class
-        //_______________________________________________________________
+
         boolean isAnagrams3 = StringUtil.isAnagramGeneric(a, b);
         boolean isAnagrams4 = StringUtil.isAnagramGeneric(c, d);
         printResult(a, b, isAnagrams3);
         printResult(c, d, isAnagrams4);
-    }
-
-    private static Character[] initArray(String s) {
-        Character[] chars = new Character[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            chars[i] = Character.valueOf(s.charAt(i));
-        }
-        return chars;
     }
 
     static void printResult(String s1, String s2, boolean isAnagrams) {
